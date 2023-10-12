24da09d9a216584c1a91e5025e2d7c8fd3c09747
Максимилиан Мамиляев <maksimilianmamilaev@Maksimilians-MacBook-Pro.local>
2023-10-12 16:23:09 +0200

    Changed info page from expanded to ListView

--- moodify/lib/info_page.dart
+++ moodify/lib/info_page.dart
@@ -12,7 +12,6 @@ class _InfoScreenState extends State<InfoScreen> {
   void _openLinkInBrowser(link) async {
     Uri uriLink = Uri.parse(link);
 
-    // Replace with your actual link
     if (await canLaunchUrl(uriLink)) {
       await launchUrl(uriLink);
     } else {
@@ -34,81 +33,91 @@ class _InfoScreenState extends State<InfoScreen> {
 
   @override
   Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: Container(
-        color: Colors.black,
-        child: Column(
+      backgroundColor: Colors.black, // Set the background color of the scaffold
+      body: Ink(
+        color: Colors.black, // Set the background color of the Ink
+        child: ListView(
           children: [
-            Expanded(
-              flex: 5,
+            Container(
+              color: Colors.black,
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Text(
-                    '''This app was made by Digital babushka to support project you can donate'''
-                        .toUpperCase(),
-                    textWidthBasis: TextWidthBasis.longestLine,
-                    style: TextStyle(
-                      color: Color.fromRGBO(255, 255, 255, 0.65),
-                      fontWeight: FontWeight.w600,
-                      letterSpacing: 9.0,
-                      fontSize: 20,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                  SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: () => _openLinkInBrowser(donationUrl),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color.fromRGBO(
-                          217, 217, 217, 1), // Button background color
-                      shape: RoundedRectangleBorder(
-                        borderRadius:
-                            BorderRadius.circular(20.0), // Rounded corners
+                  SizedBox(height: screenHeight * 0.2),
+                  SizedBox(
+                    height: screenHeight * 0.2,
+                    child: Text(
+                      '''This app was made by Digital babushka to support a project you can donate'''
+                          .toUpperCase(),
+                      textWidthBasis: TextWidthBasis.longestLine,
+                      style: TextStyle(
+                        color: Color.fromRGBO(255, 255, 255, 0.65),
+                        fontWeight: FontWeight.w600,
+                        letterSpacing: 9.0,
+                        fontSize: 20,
                       ),
+                      textAlign: TextAlign.center,
                     ),
-                    child: Text('DONATE',
-                        style: TextStyle(
-                          color: Color.fromRGBO(0, 0, 0, 1),
-                          fontWeight: FontWeight.w600,
-                          letterSpacing: 32.0,
-                          fontSize: 18,
-                        )),
                   ),
-                  SizedBox(height: 20),
-                  Text(
-                    textWidthBasis: TextWidthBasis.longestLine,
-                    '''or just share this\napp with friends'''.toUpperCase(),
-                    style: TextStyle(
-                      color: Color.fromRGBO(255, 255, 255, 0.65),
-                      fontWeight: FontWeight.w600,
-                      letterSpacing: 9.0,
-                      fontSize: 18,
+                  // SizedBox(
+                  //   height: screenHeight * 0.01,
+                  // ),
+                  SizedBox(
+                    child: ElevatedButton(
+                      onPressed: () => _openLinkInBrowser(donationUrl),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color.fromRGBO(217, 217, 217, 1),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                      ),
+                      child: Text('DONATE',
+                          style: TextStyle(
+                            color: Color.fromRGBO(0, 0, 0, 1),
+                            fontWeight: FontWeight.w600,
+                            letterSpacing: 32.0,
+                            fontSize: 18,
+                          )),
                     ),
-                    textAlign: TextAlign.center,
                   ),
-                  SizedBox(height: 40),
-                  Text(
-                    textWidthBasis: TextWidthBasis.longestLine,
-                    '''about authors'''.toUpperCase(),
-                    style: TextStyle(
-                      color: Color.fromRGBO(255, 255, 255, 0.65),
-                      fontWeight: FontWeight.w600,
-                      letterSpacing: 9.0,
-                      fontSize: 18,
+                  // SizedBox(height: screenHeight * 0.01),
+                  SizedBox(
+                    height: screenHeight * 0.2,
+                    child: Text(
+                      textWidthBasis: TextWidthBasis.longestLine,
+                      '''or just share this\napp with friends'''.toUpperCase(),
+                      style: TextStyle(
+                        color: Color.fromRGBO(255, 255, 255, 0.65),
+                        fontWeight: FontWeight.w600,
+                        letterSpacing: 9.0,
+                        fontSize: 18,
+                      ),
+                      textAlign: TextAlign.center,
                     ),
-                    textAlign: TextAlign.center,
                   ),
-                  SizedBox(height: 20),
+                  // SizedBox(height: 40),
+                  SizedBox(
+                    height: screenHeight * 0.1,
+                    child: Text(
+                      textWidthBasis: TextWidthBasis.longestLine,
+                      '''about authors'''.toUpperCase(),
+                      style: TextStyle(
+                        color: Color.fromRGBO(255, 255, 255, 0.65),
+                        fontWeight: FontWeight.w600,
+                        letterSpacing: 9.0,
+                        fontSize: 18,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  // SizedBox(height: 20),
                   ElevatedButton(
                     onPressed: () => _openLinkInBrowser(digitalBabushkaUrl),
                     style: ElevatedButton.styleFrom(
-                      backgroundColor: Color.fromRGBO(
-                          217, 217, 217, 1), // Button background color
+                      backgroundColor: Color.fromRGBO(217, 217, 217, 1),
                       shape: RoundedRectangleBorder(
-                        borderRadius:
-                            BorderRadius.circular(20.0), // Rounded corners
+                        borderRadius: BorderRadius.circular(20.0),
                       ),
                     ),
                     child: Text('digital babushka'.toUpperCase(),
@@ -116,48 +125,13 @@ class _InfoScreenState extends State<InfoScreen> {
                         style: TextStyle(
                           color: Color.fromRGBO(0, 0, 0, 1),
                           fontWeight: FontWeight.w600,
-                          // letterSpacing: 32.0,
                           fontSize: 18,
                         )),
                   ),
                 ],
               ),
             ),
-            Expanded(
-              flex: 2,
-              child: Container(
-                padding: EdgeInsets.only(bottom: 20.0),
-                decoration: BoxDecoration(
-                    border: Border(
-                  top: BorderSide(
-                      color: Color.fromRGBO(255, 255, 255, 0.65), width: 5),
-                  left: BorderSide(),
-                  right: BorderSide(),
-                  bottom: BorderSide(
-                      color: Color.fromRGBO(255, 255, 255, 0.65), width: 5),
-                )),
-                alignment: Alignment.center,
-                child: Text(
-                  textWidthBasis: TextWidthBasis.longestLine,
-                  textAlign: TextAlign.center,
-                  softWrap: true,
-                  "About Moodify".trim().toUpperCase(),
-                  style: TextStyle(
-                    color: Color.fromRGBO(255, 255, 255, 0.65),
-                    fontWeight: FontWeight.w600,
-                    // letterSpacing: 32.0,
-                    fontSize: 50,
-                  ),
-                  // textAlign: TextAlign.center,
-                ),
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Padding(
-                padding: EdgeInsets.only(top: 0, bottom: 40),
-              ),
-            ),
+            // Add new widgets here
           ],
         ),
       ),
